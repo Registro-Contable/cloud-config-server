5a86faa14e943ab42b23137729cb8a8f0156d62f
Victor Latorre <victor.latorre.herreros@gmail.com>
2020-09-19 15:33:00 +0200

    Init commit

---
+++ src/main/java/com/victorlh/registrocontable/cloudconfigserver/RegistrocontableCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.victorlh.registrocontable.cloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrocontableCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrocontableCloudConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorlh/registrocontable/cloudconfigserver/RegistrocontableCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorlh.registrocontable.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrocontableCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
