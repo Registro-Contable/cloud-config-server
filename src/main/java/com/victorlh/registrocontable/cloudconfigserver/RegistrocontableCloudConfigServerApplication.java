f4de9d4ba557e0b9ef3ee5c293f456ce95faf754
Victor Latorre <victor.latorre.herreros@gmail.com>
2020-09-20 15:26:23 +0200

    Configuracion servidor

--- src/main/java/com/victorlh/registrocontable/cloudconfigserver/RegistrocontableCloudConfigServerApplication.java
+++ src/main/java/com/victorlh/registrocontable/cloudconfigserver/RegistrocontableCloudConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.victorlh.registrocontable.cloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class RegistrocontableCloudConfigServerApplication {
 
 	public static void main(String[] args) {
