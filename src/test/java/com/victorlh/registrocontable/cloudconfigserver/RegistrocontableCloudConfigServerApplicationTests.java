ae73f8197c995754f53b0a5df28fd9a34d7be52f
Victor Latorre <victor.latorre.herreros@gmail.com>
2020-10-18 13:58:32 +0200

    Quitado fallo test

--- src/test/java/com/victorlh/registrocontable/cloudconfigserver/RegistrocontableCloudConfigServerApplicationTests.java
+++ src/test/java/com/victorlh/registrocontable/cloudconfigserver/RegistrocontableCloudConfigServerApplicationTests.java
@@ -1,7 +1,5 @@
 package com.victorlh.registrocontable.cloudconfigserver;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,7 +8,6 @@ class RegistrocontableCloudConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertTrue(false);
 	}
 
 }
